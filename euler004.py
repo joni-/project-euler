ae99b83e30a9b8e3fef8983514b979ffbff26174
Joni Nevalainen <joninevalainen@gmail.com>
2014-04-04 22:06:50 +0300

    euler004.py faster

--- euler004.py
+++ euler004.py
@@ -9,15 +9,19 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 def is_palindrome(n):
     return str(n) == str(n)[::-1]
 
-def get_largest_palindrome():
+def get_largest_palindrome(digits=3):
     palindrome = 0
-    for first in xrange(100, 1000):
-        for second in xrange(100, 1000):
+    start = 10 ** (digits-1)
+    end = 10 ** digits
+    for first in xrange(end-1, start, -1):
+        for second in xrange(end-1, start, -1):
             result = first * second
-            if is_palindrome(result) and result > palindrome:
+            if result <= palindrome:
+                break
+            elif (is_palindrome(result) and result > palindrome):
                 palindrome = result
     return palindrome
 
 if __name__ == '__main__':
-    palindrome = get_largest_palindrome()
+    palindrome = get_largest_palindrome(3)
     print palindrome
